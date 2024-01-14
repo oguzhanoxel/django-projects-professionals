fcf3c60804741ebb242423025181062413fa91f1
Oğuzhan Öksel <oguzhanoksel@hotmail.com>
2024-01-14 22:10:07 +0300

    User Registration

--- books/bookstore_project/urls.py
+++ books/bookstore_project/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
 ]
