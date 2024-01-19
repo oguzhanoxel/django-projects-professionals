9e1ff93cbeeaac5f9a6cbf41028bf32a52237bc5
Oğuzhan Öksel <oguzhanoksel@hotmail.com>
2024-01-19 23:22:49 +0300

    File/Image Uploads

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -144,6 +144,9 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Auth settings
 
 AUTH_USER_MODEL = 'users.CustomUser'
--- books/bookstore_project/urls.py
+++ books/bookstore_project/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -28,3 +30,5 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
